664ae755cc3254f3507b11713720a5848ecde658
Kenny MacLeod <kmacleod@atlassian.com>
2020-11-30 11:39:00 +0000

    DCNG-913 Add LicenseTest

---
+++ src/test/java/test/LicenseTest.java
@@ -0,0 +1,48 @@
+package test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import test.helm.Helm;
+import test.model.Product;
+
+import java.util.Map;
+
+/**
+ * Tests the various permutations of the "<product>.license" value structure in the Helm charts
+ */
+class LicenseTest {
+    private Helm helm;
+
+    @BeforeEach
+    void initHelm(TestInfo testInfo) {
+        helm = new Helm(testInfo);
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = Product.class, names = "confluence")
+    void confluence_license_secret_name(Product product) throws Exception {
+        final var resources = helm.captureKubeResourcesFromHelmChart(product, Map.of(
+                "confluence.license.secretName", "license_secret",
+                "confluence.license.secretKey", "mykey"));
+
+        resources.getStatefulSet(product.getHelmReleaseName())
+                .getContainer()
+                .getEnv()
+                .assertHasSecretRef("ATL_LICENSE_KEY", "license_secret", "mykey");
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = Product.class, names = "bitbucket")
+    void bitbucket_license_secret_name(Product product) throws Exception {
+        final var resources = helm.captureKubeResourcesFromHelmChart(product, Map.of(
+                "bitbucket.license.secretName", "license_secret",
+                "bitbucket.license.secretKey", "mykey"));
+
+        resources.getStatefulSet(product.getHelmReleaseName())
+                .getContainer()
+                .getEnv()
+                .assertHasSecretRef("SETUP_LICENSE", "license_secret", "mykey");
+    }
+}
