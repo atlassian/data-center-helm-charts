ac3d8e9206ee29e16c8b44d5f8e25a80c78da59a
Kenny MacLeod <kmacleod@atlassian.com>
2021-01-20 15:11:47 +0000

    fix shared-home-browser installation

--- src/test/scripts/shared_home_browser_install.sh
+++ src/test/scripts/shared_home_browser_install.sh
@@ -7,5 +7,5 @@ THISDIR=$(dirname "$0")
 TARGET_NAMESPACE=$1
 
 echo Starting shared home browser...
-kubectl apply -n "${TARGET_NAMESPACE}" -f "$THISDIR/../resources/shared-home-browser.yaml"
+kubectl apply -n "${TARGET_NAMESPACE}" -f "$THISDIR/../../../target/config/shared-home/shared-home-browser.yaml"
 kubectl wait -n "${TARGET_NAMESPACE}" --for=condition=Ready pod/shared-home-browser
