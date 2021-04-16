496d2471687cb7e3639f30b4e2310970976e20a7
Dylan Rathbone <drathbone@atlassian.com>
2021-04-16 15:48:03 +1000

    DCKUBE-30: Change claim to 1Gi

--- src/test/scripts/start_nfs_server.sh
+++ src/test/scripts/start_nfs_server.sh
@@ -23,12 +23,12 @@ sed -e "s/test-nfs-server/$PRODUCT_RELEASE_NAME-nfs-server/" $NFS_SERVER_YAML | 
 echo Waiting until the NFS deployment is ready...
 pod_role="$PRODUCT_RELEASE_NAME-nfs-server"
 echo Pod role is [$pod_role]
-podname=$(kubectl get pod -n "${TARGET_NAMESPACE}" -l role=$pod_role -o jsonpath="{.items[0].metadata.name}")
+pod_name=$(kubectl get pod -n "${TARGET_NAMESPACE}" -l role=$pod_role -o jsonpath="{.items[0].metadata.name}")
 echo Pod name is [$pod_name]
-kubectl wait --for=condition=ready pod -n "${TARGET_NAMESPACE}" "${podname}" --timeout=60s
+kubectl wait --for=condition=ready pod -n "${TARGET_NAMESPACE}" "${pod_name}" --timeout=60s
 
 echo Waiting for the container to stabilise...
-while ! kubectl exec -n "${TARGET_NAMESPACE}" "${podname}" -- ps -o cmd | grep 'mountd' | grep -q '/usr/sbin/rpc.mountd -N 2 -V 3'; do
+while ! kubectl exec -n "${TARGET_NAMESPACE}" "${pod_name}" -- ps -o cmd | grep 'mountd' | grep -q '/usr/sbin/rpc.mountd -N 2 -V 3'; do
   sleep 1
 done
 
