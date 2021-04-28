fe1ca1249d67e13a24e251ce7f60d65f4a84bb5b
Adam Brokes <abrokes@atlassian.com>
2021-04-28 17:18:03 +1000

    DCKUBE-30: Example implementation for NFS Server (Bitbucket)
    * DCKUBE-30: Add initial NFS server reference architecture
    
    * DCKUBE-30: Changes to utilize NFS server reference architecture
    
    * DCKUBE-30: Update the nfs script to use new reference
    
    * DCKUBE-30: Remove internal NFS server implementation
    
    * DCKUBE-30: Add outline for NFS documentation
    
    * DCKUBE-30: Update script path
    
    * DCKUBE-30: Ignore not found resources
    
    * DCKUBE-30: Switch to test image
    
    * DCKUBE-30: Update the placeholder string
    
    * DCKUBE-30: Add wait condition for deployment
    
    * DCKUBE-30: Test sleep
    
    * DCKUBE-30: Print pod role to debug selector
    
    * DCKUBE-30: List pods
    
    * DCKUBE-30: Additional logging
    
    * DCKUBE-30: Change claim to 1Gi
    
    * DCKUBE-30: Add build.component
    
    * DCKUBE-30: Fix the sed for uninstall
    
    * DCKUBE-30: Change claim to 10Gi
    
    * DCKUBE-30: Include instructions on shared storage usage
    
    * DCKUBE-30: Include instructions on shared storage usage
    
    * DCKUBE-30: Fix the target namespace for helm_uninstall.sh
    
    * DCKUBE-30: Add pod affinity for Bitbucket & NFS server
    
    * DCKUBE-30: Move affinity definition to EKS example values file
    
    * DCKUBE-236: Expand documentation
    
    * Updates to README
    
    Wording and formatting changes
    
    * DCKUBE-30: Change default for nfsPermissionFixer.enabled to false
    
    * Update codeowners
    
    Co-authored-by: Dylan Rathbone <drathbone@atlassian.com>

---
+++ src/test/scripts/start_nfs_server.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+BASEDIR=$(dirname "$0")
+
+if [ "$#" -ne 2 ]; then
+  echo "We need 2 parameters for the script - kubernetes namespace and helm release name"
+fi
+
+TARGET_NAMESPACE=$1
+PRODUCT_RELEASE_NAME=$2
+
+ARCH_EXAMPLE_DIR="$BASEDIR/../../../reference-infrastructure"
+NFS_SERVER_YAML="${ARCH_EXAMPLE_DIR}/storage/nfs/nfs-server.yaml"
+
+echo Deleting old NFS resources...
+kubectl delete -f $NFS_SERVER_YAML --ignore-not-found=true || true
+
+echo Starting NFS deployment...
+sed -e "s/test-nfs-server/$PRODUCT_RELEASE_NAME-nfs-server/" $NFS_SERVER_YAML | kubectl apply -n "${TARGET_NAMESPACE}" -f -
+
+echo Waiting until the NFS deployment is ready...
+pod_role="$PRODUCT_RELEASE_NAME-nfs-server"
+echo Pod role is [$pod_role]
+pod_name=$(kubectl get pod -n "${TARGET_NAMESPACE}" -l role=$pod_role -o jsonpath="{.items[0].metadata.name}")
+echo Pod name is [$pod_name]
+kubectl wait --for=condition=ready pod -n "${TARGET_NAMESPACE}" "${pod_name}" --timeout=60s
+
+echo Waiting for the container to stabilise...
+while ! kubectl exec -n "${TARGET_NAMESPACE}" "${pod_name}" -- ps -o cmd | grep 'mountd' | grep -q '/usr/sbin/rpc.mountd -N 2 -V 3'; do
+  sleep 1
+done
+
+echo NFS server is up and running.
