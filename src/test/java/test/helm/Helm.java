dce30f0ea1de33a9b92340d42da253424f449e55
Adam Brokes <abrokes@atlassian.com>
2021-05-05 14:15:55 +1000

    Add helm linting to test suite

--- src/test/java/test/helm/Helm.java
+++ src/test/java/test/helm/Helm.java
@@ -30,6 +30,16 @@ public final class Helm {
         return outputFile;
     }
 
+    public void assertLintedHelm(Product product) throws Exception {
+        final var process = new ProcessBuilder()
+                .command("helm", "lint",
+                        getHelmChartPath(product).toString())
+                .start();
+        final var exitCode = process.waitFor();
+        System.out.println(new String(process.getInputStream().readAllBytes()));
+        assertThat(exitCode).isEqualTo(0);
+    }
+
     private static void captureHelmTemplateOutput(Product product, Path valuesFile, Path outputFile) throws Exception {
         final var process = new ProcessBuilder()
                 .command("helm", "template",
