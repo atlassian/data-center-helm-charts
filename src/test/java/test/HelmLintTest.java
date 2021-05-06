263da4f1fb38ed6399926eaddd81a6a49ade26f4
Adam Brokes <abrokes@atlassian.com>
2021-05-06 17:12:04 +1000

    Update application versions (#116)
    * Add helm linting to test suite
    
    * Update the application versions to latest
    
    - Confluence and Bitbucket are using the latest released version
    - Jira is using latest LTS version

---
+++ src/test/java/test/HelmLintTest.java
@@ -0,0 +1,25 @@
+package test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import test.helm.Helm;
+import test.model.Product;
+
+class HelmLintTest {
+
+    private Helm helm;
+
+    @BeforeEach
+    void initHelm(TestInfo testInfo) {
+        helm = new Helm(testInfo);
+    }
+
+    @ParameterizedTest
+    @EnumSource(Product.class)
+    void helm_template_is_passing_linting(final Product product) throws Exception {
+        helm.assertLintedHelm(product);
+    }
+
+}
