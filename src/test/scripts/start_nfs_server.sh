ce2d371aa2a12265d99c19c3de6af2df7f697cfd
Adam Brokes <abrokes@atlassian.com>
2021-05-10 17:37:53 +1000

    Lower the NFS server requirements and remove storageClass

--- src/test/scripts/start_nfs_server.sh
+++ src/test/scripts/start_nfs_server.sh
@@ -11,14 +11,13 @@ fi
 TARGET_NAMESPACE=$1
 PRODUCT_RELEASE_NAME=$2
 
-ARCH_EXAMPLE_DIR="$BASEDIR/../../../reference-infrastructure"
-NFS_SERVER_YAML="${ARCH_EXAMPLE_DIR}/storage/nfs/nfs-server.yaml"
+NFS_SERVER_YAML="$BASEDIR/../infrastructure/storage/test-nfs-server.yaml"
 
 echo Deleting old NFS resources...
 kubectl delete -f $NFS_SERVER_YAML --ignore-not-found=true || true
 
 echo Starting NFS deployment...
-sed -e "s/test-nfs-server/$PRODUCT_RELEASE_NAME-nfs-server/" $NFS_SERVER_YAML | kubectl apply -n "${TARGET_NAMESPACE}" -f -
+sed -e "s/CI_PLAN_ID/$PRODUCT_RELEASE_NAME-nfs-server/" $NFS_SERVER_YAML | kubectl apply -n "${TARGET_NAMESPACE}" -f -
 
 echo Waiting until the NFS deployment is ready...
 pod_role="$PRODUCT_RELEASE_NAME-nfs-server"
