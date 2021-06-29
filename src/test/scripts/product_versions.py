86ee6ed1ed777515e731bfe90e6a22f69e29deca
nghazalibeiklar <nghazalibeiklar@atlassian.com>
2021-06-29 11:38:17 +1000

    Add some comment to cversion method as suggested in review

--- src/test/scripts/product_versions.py
+++ src/test/scripts/product_versions.py
@@ -46,12 +46,17 @@ def get_lts_version(argv):
 
 
 def cversion(version):
+	# this method converts the version to a unified string to be used for sort and compare versions correctly
+	# e.g: '7.12.1' => '00007000120000100000'
+	#      '7.3.19' => '00007000030001900000'
 	vers = version.split(".")
 	mapped_ver = ''
 	for i in range(max(len(vers)-1, 4)):
 		if len(vers) > i:
+			# add zero in left side of version part and make a fixed size of 5 for each part
 			mapped_ver += vers[i].zfill(5)
 		else:
+			# add '00000' if build/patch/minor part of version are missing
 			mapped_ver += '00000'
 	return mapped_ver
 
