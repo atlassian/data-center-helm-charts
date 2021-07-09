acdf7656766591ce93d4c9d046a76da35fd636f3
nghazalibeiklar <nghazalibeiklar@atlassian.com>
2021-07-09 15:06:29 +1000

    Added unittests

--- src/test/java/test/IngressTest.java
+++ src/test/java/test/IngressTest.java
@@ -123,4 +123,91 @@ class IngressTest {
         // This is because Connie provisions a regular ingress + an ingress for /setup paths with increased timeout
         Assertions.assertEquals(2, ingresses.size());
     }
+
+    @ParameterizedTest
+    @EnumSource(value = Product.class, names = "jira")
+    void jira_ingress_host_port(Product product) throws Exception {
+        final var resources = helm.captureKubeResourcesFromHelmChart(product, Map.of(
+                "ingress.host", "myhost.mydomain",
+                "ingress.port", "666"));
+
+        resources.getStatefulSet(product.getHelmReleaseName()).getContainer().getEnv()
+                .assertHasValue("ATL_PROXY_NAME", "myhost.mydomain")
+                .assertHasValue("ATL_PROXY_PORT", "666");
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = Product.class, names = "jira")
+    void jira_ingress_port(Product product) throws Exception {
+        final var resources = helm.captureKubeResourcesFromHelmChart(product, Map.of(
+                "ingress.host", "myhost.mydomain"));
+
+        resources.getStatefulSet(product.getHelmReleaseName()).getContainer().getEnv()
+                .assertHasValue("ATL_PROXY_NAME", "myhost.mydomain")
+                .assertHasValue("ATL_PROXY_PORT", "443");
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = Product.class, names = "jira")
+    void jira_ingress_port_http(Product product) throws Exception {
+        final var resources = helm.captureKubeResourcesFromHelmChart(product, Map.of(
+                "ingress.host", "myhost.mydomain",
+                "ingress.https", "False"));
+
+        resources.getStatefulSet(product.getHelmReleaseName()).getContainer().getEnv()
+                .assertHasValue("ATL_PROXY_NAME", "myhost.mydomain")
+                .assertHasValue("ATL_PROXY_PORT", "80");
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = Product.class, names = "jira")
+    void jira_ingress_path_contextPath(Product product) throws Exception {
+        final var resources = helm.captureKubeResourcesFromHelmChart(product, Map.of(
+                "ingress.create", "true",
+                "ingress.host", "myhost.mydomain",
+                "jira.service.contextPath", "/jira-tmp"));
+
+        final var ingresses = resources.getAll(Kind.Ingress);
+        Assertions.assertNotEquals(0, ingresses.size());
+
+        for (KubeResource ingress : ingresses) {
+            assertThat(ingress.getNode("spec", "rules").required(0).path("http").path("paths").required(0).path("path"))
+                    .hasTextContaining("/jira-tmp");
+        }
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = Product.class, names = "jira")
+    void jira_ingress_path_value(Product product) throws Exception {
+        final var resources = helm.captureKubeResourcesFromHelmChart(product, Map.of(
+                "ingress.create", "true",
+                "ingress.host", "myhost.mydomain",
+                "jira.service.contextPath", "/context",
+                "ingress.path", "/ingress"));
+
+        final var ingresses = resources.getAll(Kind.Ingress);
+        Assertions.assertNotEquals(0, ingresses.size());
+
+        for (KubeResource ingress : ingresses) {
+            assertThat(ingress.getNode("spec", "rules").required(0).path("http").path("paths").required(0).path("path"))
+                    .hasTextContaining("/ingress");
+        }
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = Product.class, names = "jira")
+    void jira_ingress_path_default(Product product) throws Exception {
+        final var resources = helm.captureKubeResourcesFromHelmChart(product, Map.of(
+                "ingress.create", "true",
+                "ingress.host", "myhost.mydomain"));
+
+        final var ingresses = resources.getAll(Kind.Ingress);
+        Assertions.assertNotEquals(0, ingresses.size());
+
+        for (KubeResource ingress : ingresses) {
+            assertThat(ingress.getNode("spec", "rules").required(0).path("http").path("paths").required(0).path("path"))
+                    .hasTextContaining("/");
+        }
+    }
+
 }
