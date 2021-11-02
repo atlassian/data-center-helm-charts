3c0dd3f64b1f7297155a781233a4d3988a83ec1e
Adam Brokes <abrokes@atlassian.com>
2021-11-02 17:59:11 +1100

    DCKUBE-731: Cleanup

--- src/test/java/test/IngressTest.java
+++ src/test/java/test/IngressTest.java
@@ -3,7 +3,6 @@ package test;
 import io.vavr.collection.Traversable;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
