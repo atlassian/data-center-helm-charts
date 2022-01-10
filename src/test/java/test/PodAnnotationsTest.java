2ca280377601e1ee1df74f0a0f3c70b1fbbf79e2
Dylan Rathbone <drathbone@atlassian.com>
2022-01-10 15:05:15 +1000

    Unit test for podAnnotations

---
+++ src/test/java/test/PodAnnotationsTest.java
@@ -0,0 +1,37 @@
+package test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import test.helm.Helm;
+import test.model.Product;
+
+import java.util.Map;
+
+import static test.jackson.JsonNodeAssert.assertThat;
+
+class PodAnnotationsTest {
+    private Helm helm;
+
+    @BeforeEach
+    void initHelm(TestInfo testInfo) {
+        helm = new Helm(testInfo);
+    }
+    
+    @ParameterizedTest
+    @EnumSource(value = Product.class, names = {"bamboo_agent"}, mode = EnumSource.Mode.EXCLUDE)
+    void service_annotations(Product product) throws Exception {
+        final var resources = helm.captureKubeResourcesFromHelmChart(product, Map.of(
+                "podAnnotations.podAnnotation1", "podOfHumBacks",
+                "podAnnotations.podAnnotation2", "podOfOrcas"
+        ));
+        
+        final var annotations = resources.getStatefulSet(product.getHelmReleaseName()).getPodMetadata().get("annotations");
+        
+        assertThat(annotations).isObject(Map.of(
+                "podAnnotation1", "podOfHumBacks",
+                "podAnnotation2", "podOfOrcas"
+        ));
+    }
+}
