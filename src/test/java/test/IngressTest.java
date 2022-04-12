bca63df27f57007e1b7972b08bc8ec7e47016973
T0bl <82589891+T0bl@users.noreply.github.com>
2022-04-12 04:08:35 +0200

    Configurable ingress proxy settings (#402)
    * jira - configurable ingress proxy settings
    
    * jira - configurable ingress proxy settings template
    
    * configurable ingress proxy settings template
    
    * fixed ingress test
    
    * Update changelog
    
    * Include PR number
    
    Co-authored-by: Dylan <drathbone@atlassian.com>

--- src/test/java/test/IngressTest.java
+++ src/test/java/test/IngressTest.java
@@ -370,7 +370,7 @@ class IngressTest {
                 "ingress.create", "true",
                 "ingress.host", "myhost.mydomain",
                 "ingress.https", "true"));
-        
+
         resources.getStatefulSet(product.getHelmReleaseName()).getContainer().getEnv()
                 .assertHasValue("ATL_BASE_URL", "https://myhost.mydomain");
     }
@@ -557,6 +557,31 @@ class IngressTest {
                 "/",
                 "/setup",
                 "/bootstrap");
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = Product.class, names = {"bamboo", "bitbucket", "confluence", "crowd", "jira"})
+    void ingress_proxy_settings(Product product) throws Exception {
+        final var resources = helm.captureKubeResourcesFromHelmChart(product, Map.of(
+                "ingress.create", "true"));
+
+        final var ingresses = resources.getAll(Kind.Ingress);
+
+        for (KubeResource ingress : ingresses) {
+                if ( ingress.getMetadata().path("name").asText().contains("-setup") ) {
+                    assertThat(ingress.getMetadata().path("annotations"))
+                        .isObject(Map.of(
+                            "nginx.ingress.kubernetes.io/proxy-connect-timeout", "300",
+                            "nginx.ingress.kubernetes.io/proxy-read-timeout", "300",
+                            "nginx.ingress.kubernetes.io/proxy-send-timeout", "300"));
+                } else {
+                    assertThat(ingress.getMetadata().path("annotations"))
+                        .isObject(Map.of(
+                            "nginx.ingress.kubernetes.io/proxy-connect-timeout", "60",
+                            "nginx.ingress.kubernetes.io/proxy-read-timeout", "60",
+                            "nginx.ingress.kubernetes.io/proxy-send-timeout", "60"));
+                }
+        }
     }
 
     private List<String> extractAllPaths(Traversable<KubeResource> ingresses) {
