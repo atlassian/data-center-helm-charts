5e9f3e5449522dfe831f3ee0d2eb9e446820b0f9
BordenIT, LLC Admin <49888529+bordenit@users.noreply.github.com>
2022-05-17 22:07:27 -0400

    Update IngressTest.java

--- src/test/java/test/IngressTest.java
+++ src/test/java/test/IngressTest.java
@@ -142,7 +142,7 @@ class IngressTest {
     }
 
     @ParameterizedTest
-    @EnumSource(value = Product.class, names = {"jira", "confluence", "bamboo"})
+    @EnumSource(value = Product.class, names = {"jira", "confluence", "bamboo", "crowd"})
     void jira_ingress_host_port(Product product) throws Exception {
         final var resources = helm.captureKubeResourcesFromHelmChart(product, Map.of(
                 "ingress.host", "myhost.mydomain",
@@ -154,7 +154,7 @@ class IngressTest {
     }
 
     @ParameterizedTest
-    @EnumSource(value = Product.class, names = {"jira", "confluence", "bamboo"})
+    @EnumSource(value = Product.class, names = {"jira", "confluence", "bamboo", "crowd"})
     void jira_ingress_port(Product product) throws Exception {
         final var resources = helm.captureKubeResourcesFromHelmChart(product, Map.of(
                 "ingress.host", "myhost.mydomain"));
@@ -165,7 +165,7 @@ class IngressTest {
     }
 
     @ParameterizedTest
-    @EnumSource(value = Product.class, names = {"jira", "confluence", "bamboo"})
+    @EnumSource(value = Product.class, names = {"jira", "confluence", "bamboo", "crowd"})
     void jira_ingress_port_http(Product product) throws Exception {
         final var resources = helm.captureKubeResourcesFromHelmChart(product, Map.of(
                 "ingress.host", "myhost.mydomain",
