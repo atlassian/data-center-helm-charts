2df7ebd51e3f877ffaa6cf3483f2c10e69ef224a
yivantsov-atlassian <52448429+bianchi2@users.noreply.github.com>
2023-05-18 08:14:28 +1000

    Handle empty panels of row type

--- src/main/scripts/process-dashboard.py
+++ src/main/scripts/process-dashboard.py
@@ -45,30 +45,31 @@ data['templating'] = templating
 # Finding and replacing values
 def process_panels(panels):
     for panel in panels:
-        print('Processing panel ' + panel['title'])
-        for target in panel.get('targets', []):
-            print('Processing expression ' + target['expr'])
-            # make sure datasource is templated and there is no hardcoded ID
-            datasource = json.loads('{"type":"prometheus","uid":"$datasource"}')
-            target['datasource'] = datasource
-            # we don't need product labels in K8s
-            remove_product_label = re.sub(r'product="[^"]*"', '', target['expr'])
-            target['expr'] = remove_product_label
-            # we don't need instance labels in K8s because pod IPs are dynamic
-            remove_instance_label = re.sub(r'instance="[^"]*"', '', target['expr'])
-            target['expr'] = remove_instance_label
-            # In the legend, a pod makes more sense than instance
-            # which is IP that will change. Replace {{instance}} with {{ pod }}, regex is used
-            # to match {{ instance }}, {{instance }} and {{ instance}}
-            replace_instance_with_pod = re.sub(r'{{\s*instance\s*}}', '{{ pod }}', target['legendFormat'])
-            target['legendFormat'] = replace_instance_with_pod
-            expr = target['expr']
-            expr = expr.replace('{{instance}}', '{{ pod }}')
-            # we need namespace and service filters in all expressions to make dashboards flexible
-            # we assume that the expression has the following format: expression{},
-            # i.e. it has an opening curly bracket. Make sure all expressions in the source json have those
-            expr = expr.replace('{', '{namespace="$namespace", service="$service", ')
-            target['expr'] = expr
+        if panel['type'] != 'row':
+            print('Processing panel ' + panel['title'])
+            for target in panel.get('targets', []):
+                print('Processing expression ' + target['expr'])
+                # make sure datasource is templated and there is no hardcoded ID
+                datasource = json.loads('{"type":"prometheus","uid":"$datasource"}')
+                target['datasource'] = datasource
+                # we don't need product labels in K8s
+                remove_product_label = re.sub(r'product="[^"]*"', '', target['expr'])
+                target['expr'] = remove_product_label
+                # we don't need instance labels in K8s because pod IPs are dynamic
+                remove_instance_label = re.sub(r'instance="[^"]*"', '', target['expr'])
+                target['expr'] = remove_instance_label
+                # In the legend, a pod makes more sense than instance
+                # which is IP that will change. Replace {{instance}} with {{ pod }}, regex is used
+                # to match {{ instance }}, {{instance }} and {{ instance}}
+                replace_instance_with_pod = re.sub(r'{{\s*instance\s*}}', '{{ pod }}', target['legendFormat'])
+                target['legendFormat'] = replace_instance_with_pod
+                expr = target['expr']
+                expr = expr.replace('{{instance}}', '{{ pod }}')
+                # we need namespace and service filters in all expressions to make dashboards flexible
+                # we assume that the expression has the following format: expression{},
+                # i.e. it has an opening curly bracket. Make sure all expressions in the source json have those
+                expr = expr.replace('{', '{namespace="$namespace", service="$service", ')
+                target['expr'] = expr
 
         nested_panels = panel.get('panels', [])
         if nested_panels:
@@ -80,3 +81,4 @@ process_panels(data['panels'])
 print('Saving the file to ' + args.dest)
 with open(args.dest, 'w') as file:
     json.dump(data, file, indent=2)
+ 
