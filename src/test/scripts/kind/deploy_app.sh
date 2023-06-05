8c27355f3fd8b3b258dc48eccd8407aa459722cc
yivantsov-atlassian <52448429+bianchi2@users.noreply.github.com>
2023-06-06 08:58:13 +1000

    Update src/test/scripts/kind/deploy_app.sh
    Co-authored-by: D∇L∆N <drathbone@atlassian.com>

--- src/test/scripts/kind/deploy_app.sh
+++ src/test/scripts/kind/deploy_app.sh
@@ -107,21 +107,21 @@ verify_metrics() {
 
 verify_dashboards() {
   echo "[INFO]: Verifying ConfigMaps with Grafana dashboards"
-  DASHBPOARDS_COUNT=$(find src/main/charts/"${DC_APP}"/grafana-dashboards  -name 'bitbucket-mesh' -prune -o -type f -print | wc -l)
+  DASHBOARDS_COUNT=$(find src/main/charts/"${DC_APP}"/grafana-dashboards -name 'bitbucket-mesh' -prune -o -type f -print | wc -l)
   CONFIGMAPS_COUNT=$(kubectl get cm -l=grafana_dashboard=dc_monitoring -n atlassian --no-headers -o custom-columns=":metadata.name" | wc -l)
-  if [ "${DASHBPOARDS_COUNT}" -ne "${CONFIGMAPS_COUNT}" ]; then
-    echo "[ERROR]: Count does not match! Dashboards count is ${DASHBPOARDS_COUNT}, configmaps count is ${CONFIGMAPS_COUNT}"
+  if [ "${DASHBOARDS_COUNT}" -ne "${CONFIGMAPS_COUNT}" ]; then
+    echo "[ERROR]: Count does not match! Dashboards count is ${DASHBOARDS_COUNT}, configmaps count is ${CONFIGMAPS_COUNT}"
     echo -e "[ERROR]: ConfigMaps with grafana_dashboard=dc_monitoring label in atlassian namespace:\n"
     kubectl get cm -l=grafana_dashboard=dc_monitoring -n atlassian --no-headers -o custom-columns=":metadata.name"
     exit 1
   fi
 
-  DASHBOARDS=($(find src/main/charts/${DC_APP}/grafana-dashboards  -name 'bitbucket-mesh' -prune -o -type f -print))
+  DASHBOARDS=$(find src/main/charts/"${DC_APP}"/grafana-dashboards -name 'bitbucket-mesh' -prune -o -type f -print)
   for dashboard in "${DASHBOARDS[@]}"; do
     echo "[INFO]: Comparing $dashboard with its respective ConfigMap"
     dashboard_json=$(cat "${dashboard}")
     file_name=$(basename "${dashboard}" |cut -d '.' -f 1)
-    cm_data=$(kubectl get cm/${DC_APP}-${file_name}-dashboard -n atlassian -o jsonpath="{.data.${DC_APP}-atlassian-${file_name}\.json}")
+    cm_data=$(kubectl get cm/"${DC_APP}"-"${file_name}"-dashboard -n atlassian -o jsonpath="{.data.${DC_APP}-atlassian-${file_name}\.json}")
     if [ "${dashboard_json}" != "${cm_data}" ]; then
         echo "[ERROR]: ConfigMap ${DC_APP}-${file_name}-dashboard data isn't identical to ${file_name}.json"
         echo "******************************************************************"
