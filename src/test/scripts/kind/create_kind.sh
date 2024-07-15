627bf69fdb82c6adcba37d83b479d76ce89172bd
yivantsov-atlassian <52448429+bianchi2@users.noreply.github.com>
2024-07-15 12:07:26 +1000

    Explicitly check if user is root when installing KinD (#830)
    * Identify user
    
    * Identify user
    
    * Identify user
    
    ---------
    
    Co-authored-by: Yevhen Ivantsov <yivantsov@atlassian.com>

--- src/test/scripts/kind/create_kind.sh
+++ src/test/scripts/kind/create_kind.sh
@@ -8,15 +8,29 @@ export KIND_VERSION="${KIND_VERSION:-v0.23.0}"
 
 if [ -z "${SKIP_DOWNLOAD_KIND}" ]; then
   echo "[INFO]: Downloading KinD ${KIND_VERSION}"
-  curl -Lo ./kind "https://kind.sigs.k8s.io/dl/${KIND_VERSION}/kind-$(uname)-amd64"
+  curl -sLo ./kind "https://kind.sigs.k8s.io/dl/${KIND_VERSION}/kind-$(uname)-amd64"
   chmod +x ./kind
-  SUDO=$(which sudo)
-  ${SUDO} mv ./kind /usr/local/bin/kind
+  if [ "$(id -u)" -ne 0 ]; then
+    echo "[INFO]: User is not root. Attempting to move kind to PATH with sudo"
+    SUDO=$(which sudo)
+    ${SUDO} mv ./kind /usr/local/bin/kind 
+  else
+    echo "[INFO]: User is root. Copying kind to PATH"
+    mv ./kind /usr/local/bin/kind
+  fi
 fi
 
-echo "[INFO]: Using config file src/test/config/kind/kind-config.yml"
+if command -v kind >/dev/null 2>&1; then
+  echo "[INFO]: Installed KinD: $(kind version)"
+  echo "[INFO]: Kubernetes version: ${K8S_VERSION}"
+  echo "[INFO]: Creating 'atl-kind' cluster using config file src/test/config/kind/kind-config.yml"
+  kind create cluster --name=atl-kind \
+                      --image=kindest/node:${K8S_VERSION} \
+                      --config=src/test/config/kind/kind-config.yml \
+                      --wait=5m
+else
+  echo "[ERROR]: Failed to successfully install KinD. Exiting"
+  exit 1
+fi
 
-kind create cluster --name=atl-kind \
-                    --image=kindest/node:${K8S_VERSION} \
-                    --config=src/test/config/kind/kind-config.yml \
-                    --wait=5m
+
